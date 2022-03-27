8e8cefede8f7216b8017a094a62923989b9e4d67
Thomas Perret <thomas.perret@iut-rodez.fr>
2022-03-27 21:28:27 +0200

    jeu p4.1

--- model/jeuModel.php
+++ model/jeuModel.php
@@ -4,10 +4,11 @@ require '../bdd/connexion.php';
 
 class jeuModel extends connexion {
     function recupererGrille($idPartie) {
-        $req = $this->connect()->prepare("SELECT hauteur, largeur, grille FROM partie WHERE id = ?");
+        $req = $this->connect()->prepare("SELECT id, hauteur, largeur, grille FROM partie WHERE id = ?");
         try {
             $req->execute(array($idPartie));
             while ($row = $req->fetch()) {
+                $_SESSION['idPartie'] = $row['id'];
                 $_SESSION['hauteurG'] = $row['hauteur'];
                 $_SESSION['largeurG'] = $row['largeur'];
                 $_SESSION['grille'] = $row['grille'];
--- vue/jeuView.php
+++ vue/jeuView.php
@@ -2,21 +2,22 @@
 
 <?php ob_start(); ?>
 
-
+<div><h2><?php echo 'id de partie : ' . $_SESSION["idPartie"]; ?></h2></div>
 <div class="container text-center fa-4x">
     <?php // génération de la grille
-    for ($row = 0; $row < $hauteur; $row++){
+    for ($row = 0; $row < $hauteur; $row++) {
         echo '<div class="row" style="margin: -15px;">';
         for ($col = 0; $col < $largeur; $col++) {
-            echo '<div data- class="col">
+            echo '<div onclick="' . $col . $row . '" class="col">
                 <i class="fas fa-circle"></i>
             </div>';
+
+            echo '<script></script>';
         }
         echo '</div>';
     }
     ?>
 </div>
-
 
 
 <?php $content = ob_get_clean(); ?>
